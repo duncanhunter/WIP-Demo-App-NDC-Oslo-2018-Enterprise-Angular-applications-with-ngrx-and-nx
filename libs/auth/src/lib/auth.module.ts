a88fb8fe930b71f7cc6ea9948f4c1e15fcc6eb0f
duncanhunter <duncan@duncanhunter.com.au>
2018-06-12 11:31:18 +0200

    add container and presentational

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { LoginComponent } from './containers/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 export const authRoutes: Route[] = [];
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [LoginComponent, LoginFormComponent]
 })
 export class AuthModule {}
